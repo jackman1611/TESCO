7cd639f6c9274d4381ef2c63ed0b5b8baea9b326
Jack <Jack@JACK>
2017-05-17 20:05:20 -0500

    Primera modificacion 3

--- Git/src/git/Git.java
+++ Git/src/git/Git.java
@@ -1,16 +1,9 @@
 package git;
 
-/**
- *
- * @author Jack
- */
 public class Git {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
