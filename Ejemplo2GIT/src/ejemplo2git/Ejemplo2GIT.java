1f149eb47adf9614a1a8bd5fc26b20862026c691
jackman1611 <jack.kindap@gmail.com>
2017-05-24 22:23:55 -0500

    Update Ejemplo2GIT.java
    Modificado des github

--- Ejemplo2GIT/src/ejemplo2git/Ejemplo2GIT.java
+++ Ejemplo2GIT/src/ejemplo2git/Ejemplo2GIT.java
@@ -5,6 +5,7 @@ public class Ejemplo2GIT {
     public static void main(String[] args) {
         System.out.println("Cambio esto");
         System.out.println("Modificacion 2");
+        /* Nota agregada desde GITHUB */
     }
     
 }
